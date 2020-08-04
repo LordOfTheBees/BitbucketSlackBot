9020fa56e6edbb1458b18686cec8c16f9dde2bee
Slava Obrezkov <slava.obrezkov@zennolab.com>
2020-08-04 09:32:03 +0300

    Add log information

--- Web/Middlewares/TestMiddleware.cs
+++ Web/Middlewares/TestMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,17 +10,45 @@ namespace BitbucketSlackBot.Middlewares
 {
     public class TestMiddleware
     {
+        ILogger<TestMiddleware> _logger;
         private RequestDelegate _next;
 
-        public TestMiddleware(RequestDelegate next)
+        public TestMiddleware(RequestDelegate next, ILogger<TestMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-            Console.WriteLine(context.Request.Path);
-            Console.WriteLine(string.Join('|', context.Request.Query));
+            _logger.LogInformation("=============================================================\r\n========================Request==============================");
+
+            _logger.LogInformation(string.Format("{0} | {1}", context.Request.Path, string.Join('|', context.Request.Query)));
+
+            try
+            {
+                var streamReader = new StreamReader(context.Request.Body);
+                var body = streamReader.ReadToEnd();
+                _logger.LogInformation(body);
+
+                try
+                {
+                    streamReader = new StreamReader(context.Request.Body);
+                    var tmpBody = streamReader.ReadToEnd();
+                    _logger.LogInformation(body == tmpBody ? "All Good" : "No Good");
+                }
+                catch
+                {
+                    _logger.LogInformation("No Good");
+                }
+            }
+            catch
+            {
+                _logger.LogInformation("No Body");
+            }
+
+            _logger.LogInformation("=============================================================\r\n=============================================================");
+
             await _next.Invoke(context);
         }
     }
