eb938a603d2777e96c609a2950821b5032aaada2
Slava Obrezkov <slava.obrezkov@zennolab.com>
2020-08-04 07:42:23 +0300

    Add developer page in release

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -40,10 +40,7 @@ namespace BitbucketSlackBot
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseMiddleware<TestMiddleware>();
 
