40b5be1b178d59dfab2393645b8b22c49cbf50a3
Slava Obrezkov <slava.obrezkov@zennolab.com>
2020-08-04 07:27:58 +0300

    Add actions file for Azure

---
+++ Web/Controllers/Slack/SRepositoryController.cs
@@ -0,0 +1,40 @@
+﻿using Bitbucket.API.AuthenticationClients;
+using Bitbucket.API.BitbucketClients;
+using Bitbucket.API.Data.EnumTypes;
+using Bitbucket.API.Interfaces.BitbucketClients;
+using BitbucketSlackBot.Configs;
+using BitbucketSlackBot.Controllers.Bitbucket;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitbucketSlackBot.Controllers.Slack
+{
+    [Route("Slack/Repository")]
+    public class SRepositoryController: ControllerBase
+    {
+        private readonly ILogger<SRepositoryController> _logger;
+
+        IBitbucketRepositoryClient _bitbucketClient = null;
+        BitbucketAuthConfig _authConfig = null;
+        BitbucketRepoConfig _repoConfig = null;
+
+        public SRepositoryController(ILogger<SRepositoryController> logger, IOptions<BitbucketAuthConfig> authConfig, IOptions<BitbucketRepoConfig> repoConfig)
+        {
+            _logger = logger;
+            _authConfig = authConfig.Value;
+            _repoConfig = repoConfig.Value;
+            _bitbucketClient = new BitbucketClient(new BasicAuthenticationClient(_authConfig.ClientID, _authConfig.ClientPassword));
+        }
+
+        [HttpGet]
+        public async Task<object> GetRepository()
+        {
+            return (await _bitbucketClient.GetRepositories(RepositoryRole.Member)).Select(repo => repo.FullName);
+        }
+    }
+}
